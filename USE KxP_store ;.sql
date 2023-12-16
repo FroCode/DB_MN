09fd2b1a33ca9eb7e2695aa593d3e63fa1f6a916
Haddouche Maamar <frocodeof@gmail.com>
2023-12-16 23:53:34 +0100

    Update

--- USE KxP_store ;.sql
+++ USE KxP_store ;.sql
@@ -2,4 +2,4 @@ USE KxP_store ;
 
 SELECT order_id , products.product_id , quantity , products.unit_price , quantity * products.unit_price AS total_price
 FROM order_items
-JOIN products ON order_items.product_id = products.product_id
+
